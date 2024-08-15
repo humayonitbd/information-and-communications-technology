e34908518428534d2ca642cc50627dec1d20a96b
humayonitbd <humayonforid44@gmail.com>
2024-08-15 16:30:29 +0600

    code updated

--- src/pages/OurService/OurService.tsx
+++ src/pages/OurService/OurService.tsx
@@ -159,7 +159,7 @@ const OurService = () => {
         className="py-10 bg-gradient-to-r from-blue-50 to-gray-100"
       >
         <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {services?.map((service) => (
               <div
                 key={service.id}
